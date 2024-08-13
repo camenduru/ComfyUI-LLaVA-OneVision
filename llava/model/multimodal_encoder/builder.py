7150506dce6cc66f85dd823215bc129d095a4440
camenduru <54370274+camenduru@users.noreply.github.com>
2024-08-13 23:05:24 +0300

    from_local

--- llava/model/multimodal_encoder/builder.py
+++ llava/model/multimodal_encoder/builder.py
@@ -14,19 +14,20 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
     vision_tower = getattr(vision_tower_cfg, "mm_vision_tower", getattr(vision_tower_cfg, "vision_tower", None))
     is_absolute_path_exists = os.path.exists(vision_tower)
     use_s2 = getattr(vision_tower_cfg, "s2", False)
-    if is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion") or "ShareGPT4V" in vision_tower:
-        if use_s2:
-            return CLIPVisionTowerS2(vision_tower, args=vision_tower_cfg, **kwargs)
-        else:
-            return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
-    elif "siglip" in vision_tower:
-        return SigLipVisionTower(vision_tower, vision_tower_cfg=vision_tower_cfg, **kwargs)
-    elif vision_tower.startswith("hf:"):
-        return HFVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
-    elif vision_tower in ["imagebind_huge"]:
-        return ImageBindWrapper(vision_tower, args=vision_tower_cfg, **kwargs)
-    elif vision_tower.startswith("open_clip_hub"):
-        return OpenCLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+    return SigLipVisionTower("/content/ComfyUI/models/LLM/siglip-so400m-patch14-384", vision_tower_cfg=vision_tower_cfg, **kwargs)
+    # if is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion") or "ShareGPT4V" in vision_tower:
+    #     if use_s2:
+    #         return CLIPVisionTowerS2(vision_tower, args=vision_tower_cfg, **kwargs)
+    #     else:
+    #         return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+    # elif "siglip" in vision_tower:
+    #     return SigLipVisionTower(vision_tower, vision_tower_cfg=vision_tower_cfg, **kwargs)
+    # elif vision_tower.startswith("hf:"):
+    #     return HFVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+    # elif vision_tower in ["imagebind_huge"]:
+    #     return ImageBindWrapper(vision_tower, args=vision_tower_cfg, **kwargs)
+    # elif vision_tower.startswith("open_clip_hub"):
+    #     return OpenCLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
     # elif "internal-eva" in vision_tower.lower() or "eva02" in vision_tower.lower():
     #     return EvaClipVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
     # elif vision_tower in ["EVA-CLIP-8B", "EVA-CLIP-8B-plus"]:
